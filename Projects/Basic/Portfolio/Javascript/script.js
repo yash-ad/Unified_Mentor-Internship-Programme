6e0641b055f80e09d6e5de438ac90a36fd5b99ce
Yash Dandnaik <yashrajdandnaik.com>
2024-02-15 10:52:39 +0530

    tilting the profile pic using javascript tilt library

--- Projects/Basic/Portfolio/Javascript/script.js
+++ Projects/Basic/Portfolio/Javascript/script.js
@@ -1,8 +1,13 @@
 //Lets add javascript functioning for toggling the menu
-
 const toggleMenu = () => {
     const menu = document.querySelector(".menu-links"); // Select the first element with class "menu-links"
     const icon = document.querySelector(".hamburger-icon"); // Select the first element with class "hamburger-icon"
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 };
+
+
+//Lets tilt the profile picture image using Javascript library tilt.js
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    max: 15,
+});
