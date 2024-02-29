a2c4f704b0ad895472cc853dbb4829c7c7fd8b90
Yash Dandnaik <yashrajdandnaik.com>
2024-02-29 17:58:28 +0530

    Removed unnecessary files

--- Projects/Basic/Portfolio/Javascript/script.js
+++ Projects/Basic/Portfolio/Javascript/script.js
@@ -185,10 +185,6 @@ srtop.reveal('.education .box', { interval: 200 });
 /* SCROLL PROJECTS */
 srtop.reveal('.work .box', { interval: 200 });
 
-/* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline', { delay: 400 });
-srtop.reveal('.experience .timeline .container', { interval: 400 });
-
 /* SCROLL CONTACT */
-srtop.reveal('.contact .container', { delay: 400 });
-srtop.reveal('.contact .container .form-group', { delay: 400 });
+srtop.reveal('#contact .contact-form', { delay: 400 });
+srtop.reveal('#contact .contact-info-container', { delay: 300 });
