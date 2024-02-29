5f07c8bc2f1c63934011a012fdb6714f3550a64d
Yash Dandnaik <yashrajdandnaik.com>
2024-02-29 17:26:41 +0530

    implementing reveal animation and its features

--- Projects/Basic/Portfolio/Javascript/script.js
+++ Projects/Basic/Portfolio/Javascript/script.js
@@ -125,7 +125,7 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 });
 
 
-// <!-- typed js effect starts -->
+// typed js effect
 var typed = new Typed(".typing-text", {
     strings: ["Frontend developer"],
     loop: true,
@@ -134,4 +134,61 @@ var typed = new Typed(".typing-text", {
     backDelay: 500,
 });
 
+// Visiting to the profile and its visibility:-
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Portfolio | Yash Dandnaik";
+        }
+        else {
+            document.title = "Come Back To Portfolio"
+        }
+    });
 
+
+    /* ===== SCROLL REVEAL ANIMATION ===== */
+const srtop = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1000,
+    reset: true
+});
+
+/* SCROLL HOME */
+srtop.reveal('.home .content h3', { delay: 200 });
+srtop.reveal('.home .content p', { delay: 200 });
+srtop.reveal('.home .content .btn', { delay: 200 });
+
+srtop.reveal('.home .image', { delay: 400 });
+srtop.reveal('.home .linkedin', { interval: 600 });
+srtop.reveal('.home .github', { interval: 800 });
+srtop.reveal('.home .twitter', { interval: 1000 });
+srtop.reveal('.home .telegram', { interval: 600 });
+srtop.reveal('.home .instagram', { interval: 600 });
+srtop.reveal('.home .dev', { interval: 600 });
+
+/* SCROLL ABOUT */
+srtop.reveal('.about .content h3', { delay: 200 });
+srtop.reveal('.about .content .tag', { delay: 200 });
+srtop.reveal('.about .content p', { delay: 200 });
+srtop.reveal('.about .content .box-container', { delay: 200 });
+srtop.reveal('.about .content .resumebtn', { delay: 200 });
+
+
+/* SCROLL SKILLS */
+srtop.reveal('.skills .container', { interval: 200 });
+srtop.reveal('.skills .container .bar', { delay: 400 });
+
+/* SCROLL EDUCATION */
+srtop.reveal('.education .box', { interval: 200 });
+
+/* SCROLL PROJECTS */
+srtop.reveal('.work .box', { interval: 200 });
+
+/* SCROLL EXPERIENCE */
+srtop.reveal('.experience .timeline', { delay: 400 });
+srtop.reveal('.experience .timeline .container', { interval: 400 });
+
+/* SCROLL CONTACT */
+srtop.reveal('.contact .container', { delay: 400 });
+srtop.reveal('.contact .container .form-group', { delay: 400 });
