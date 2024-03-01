c6714201c64a4639fe4cb8eacfb6c1f5742bb613
Yash Dandnaik <yashrajdandnaik.com>
2024-03-01 14:03:28 +0530

    Make Working Contact Form With JavaScript | Receive Form Data On Email

--- Projects/Basic/Portfolio/Javascript/script.js
+++ Projects/Basic/Portfolio/Javascript/script.js
@@ -1,131 +1,123 @@
-//Lets create a function for required input fields:-
-
-//Firstly lets create variables fetching from html:-
-//Variable declarations:-(To display error)
+// Variable declarations for error messages
 let nameError = document.getElementById('name-error');
 let emailError = document.getElementById('email-error');
 let subjectError = document.getElementById('subject-error');
 let messageError = document.getElementById('message-error');
 let submitError = document.getElementById('submit-error');
 
-//Function declarations:-
-function validateName(){
+// Function to validate name input
+function validateName() {
     let name = document.getElementById('contact-name').value;
 
-    if(name.length === 0){
-nameError.innerHTML = "*Name is required";
-return false;
+    if (name.length === 0) {
+        nameError.innerHTML = "*Name is required";
+        return false;
     }
 
-if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-    nameError.innerHTML = "Fullname is required"
-    return false;
-}
-    
-    else{
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = "Fullname is required";
+        return false;
+    } else {
         nameError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
         return true;
     }
-};
+}
 
+// Function to validate email input
+function validateEmail() {
+    let email = document.getElementById('contact-email').value;
 
-
- function validateEmail(){
-    let email = document.getElementById('contact-email').value; //It will store the value, written in the input of variable called `email` 
-
-    if(email.length === 0){
+    if (email.length === 0) {
         emailError.innerHTML = "*Email is required";
         return false;
-            };
-    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){ //It should contain alphabet and it may contain `.` or `_` and it can also contain any numeric and thereafter `@` thereafter some aplhabets and then `.` another alphabet it could be two,three or four characters.
+    }
+
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
         emailError.innerHTML = "Email Invalid";
         return false;
-    }
-    else{
-       emailError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+    } else {
+        emailError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
         return true;
     }
- };
-
-function validateSubject(){
-let subject = document.getElementById('contact-subject').value;
-
-if(subject.length === 0){
-    subjectError.innerHTML = "*Subject is required"
-    return false;
-}
-else{
- subjectError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-     return true;
- }
 }
 
- function validateMessage(){
-let message = document.getElementById('contact-message').value;
-let requiredCharacters = 30; //30 characters is required.
-let remainingCharacters = requiredCharacters - message.length; //left (remaining) characters while user typing - message (How much characters is left)
- 
-if(message == 0){
-    messageError.innerHTML = "*Message is required"
-    return false;
- }
-if(remainingCharacters > 0){
-    messageError.innerHTML = remainingCharacters + ' more characters are required';
-    return false;
+// Function to validate subject input
+function validateSubject() {
+    let subject = document.getElementById('contact-subject').value;
+
+    if (subject.length === 0) {
+        subjectError.innerHTML = "*Subject is required";
+        return false;
+    } else {
+        subjectError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+        return true;
+    }
 }
+
+// Function to validate message input
+function validateMessage() {
+    let message = document.getElementById('contact-message').value;
+    let requiredCharacters = 30;
+    let remainingCharacters = requiredCharacters - message.length;
+
+    if (message == 0) {
+        messageError.innerHTML = "*Message is required";
+        return false;
+    }
+    
+    if (remainingCharacters > 0) {
+        messageError.innerHTML = remainingCharacters + ' more characters are required';
+        return false;
+    }
+    
     messageError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
     return true;
-
- }
-
- function validateForm(){
-    if(!validateName()  || !validateEmail() || !validateSubject || !validateMessage()){
-        submitError.style.display = 'block';
-        submitError.innerHTML = "*To submit it, please fix the error."
-        setTimeout(()=>{
-            submitError.style.display = "none";
-        },3000)
-        return false;
 }
-else {
-    clearForm(); // Assuming you have a clearForm() function
+
+// Function to validate the entire form
+function validateForm() {
+    if (!validateName() || !validateEmail() || !validateSubject() || !validateMessage()) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = "*To submit it, please fix the error.";
+        setTimeout(() => {
+            submitError.style.display = "none";
+        }, 3000);
+        return false;
+    } else {
+        clearForm();
         setTimeout(() => {
             alert("Form submitted successfully");
-            document.getElementById('myForm').submit(); // Assuming your form has the ID 'myForm'
+            document.getElementById('myForm').submit();
         }, 0);
         return true;
+    }
 }
-};
 
-
-function clearForm(){
-    document.getElementById('myForm').reset(); // Assuming your form has the ID 'myForm'
+// Function to clear the form
+function clearForm() {
+    document.getElementById('myForm').reset();
     let checkIcons = document.querySelectorAll('.fa-solid.fa-circle-check');
-    
-   checkIcons.forEach((icon)=>{
-icon.parentNode.removeChild(icon)
-   })
-};
+    checkIcons.forEach(icon => {
+        if (icon.parentNode) {
+            icon.parentNode.removeChild(icon);
+        }
+    });
+}
 
-
-//Lets add jquery code functioning for toggling the menu:-
+// Toggle menu functionality using jQuery
 $(document).ready(function () {
-
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
-
 });
 
-
-//Lets tilt the profile picture image using Javascript library tilt.js:-
+// Tilt the profile picture image using VanillaTilt
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
     max: 15,
 });
 
-
-// typed js effect
+// Typed.js effect
 var typed = new Typed(".typing-text", {
     strings: ["Frontend developer"],
     loop: true,
@@ -134,19 +126,16 @@ var typed = new Typed(".typing-text", {
     backDelay: 500,
 });
 
-// Visiting to the profile and its visibility:-
-document.addEventListener('visibilitychange',
-    function () {
-        if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Yash Dandnaik";
-        }
-        else {
-            document.title = "Come Back To Portfolio"
-        }
-    });
+// Change document title based on visibility
+document.addEventListener('visibilitychange', function () {
+    if (document.visibilityState === "visible") {
+        document.title = "Portfolio | Yash Dandnaik";
+    } else {
+        document.title = "Come Back To Portfolio";
+    }
+});
 
-
-    /* ===== SCROLL REVEAL ANIMATION ===== */
+// Scroll reveal animation
 const srtop = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -154,11 +143,10 @@ const srtop = ScrollReveal({
     reset: true
 });
 
-/* SCROLL HOME */
+// Scroll reveal animations for various sections
 srtop.reveal('.home .content h3', { delay: 200 });
 srtop.reveal('.home .content p', { delay: 200 });
 srtop.reveal('.home .content .btn', { delay: 200 });
-
 srtop.reveal('.home .image', { delay: 400 });
 srtop.reveal('.home .linkedin', { interval: 600 });
 srtop.reveal('.home .github', { interval: 800 });
@@ -166,25 +154,64 @@ srtop.reveal('.home .twitter', { interval: 1000 });
 srtop.reveal('.home .telegram', { interval: 600 });
 srtop.reveal('.home .instagram', { interval: 600 });
 srtop.reveal('.home .dev', { interval: 600 });
-
-/* SCROLL ABOUT */
 srtop.reveal('.about .content h3', { delay: 200 });
 srtop.reveal('.about .content .tag', { delay: 200 });
 srtop.reveal('.about .content p', { delay: 200 });
 srtop.reveal('.about .content .box-container', { delay: 200 });
 srtop.reveal('.about .content .resumebtn', { delay: 200 });
-
-
-/* SCROLL SKILLS */
 srtop.reveal('.skills .container', { interval: 200 });
 srtop.reveal('.skills .container .bar', { delay: 400 });
-
-/* SCROLL EDUCATION */
-srtop.reveal('.education .box', { interval: 200 });
-
-/* SCROLL PROJECTS */
 srtop.reveal('.work .box', { interval: 200 });
-
-/* SCROLL CONTACT */
 srtop.reveal('#contact .contact-form', { delay: 400 });
 srtop.reveal('#contact .contact-info-container', { delay: 300 });
+
+// Function to add 'active' class to the navigation link corresponding to the current section
+function highlightActiveNavLink() {
+    var navLinks = document.querySelectorAll('.navbar ul li a');
+
+    document.querySelectorAll('section').forEach(function(section) {
+      var top = section.offsetTop - 50;
+      var bottom = top + section.offsetHeight;
+
+      if (window.scrollY >= top && window.scrollY < bottom) {
+        navLinks.forEach(function(navLink) {
+          navLink.classList.remove('active');
+        });
+        
+        var correspondingNavLink = document.querySelector('.navbar ul li a[href="#' + section.id + '"]');
+        if (correspondingNavLink) {
+          correspondingNavLink.classList.add('active');
+        }
+      }
+    });
+}
+
+// Add event listener for scrolling
+window.addEventListener('scroll', highlightActiveNavLink);
+
+// Call the function initially to highlight the active navigation link on page load
+highlightActiveNavLink();
+
+
+// Function to send an email:-
+
+function sendEmail(){
+let name = document.getElementById('contact-name').value;
+let email = document.getElementById('contact-email').value;
+let subject = document.getElementById('contact-subject').value;
+let message = document.getElementById('contact-message').value;
+let finalmessage = `Name : ${name} <br>  Email : ${email} <br>  Subject : ${subject} <br>  Message : ${message} <br>`;
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "yashrajdandnaik@gmail.com",
+    Password : "91D7322AB6349247A97244EAFCBB92EA6CFF",
+    To : 'yashdandnaik078@gmail.com',
+    From : email,
+    Subject :subject,
+    Body :finalmessage
+}).then(
+  message => alert(message)
+);
+
+
+}
